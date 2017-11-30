2efacaa49a47b07244ef5d135d145231ae566a7e
jmartinez996 <jmartinez2015@alu.uct.cl>
2017-11-29 23:42:56 -0400

    cago el pdf vendor jolaperra

---
+++ Proyecto matriculas/View/matricula/matricula-pdf.php
@@ -0,0 +1,11 @@
+<ol class="breadcrumb">
+  <li><a href="?c=matricula">Matriculas</a></li>
+  <li class="active"><?php echo $pdr->id_matricula != null ? $pdr->id_matricula : 'Nuevo Registro'; ?></li>
+</ol>
+
+
+<?php
+ 
+?>
+
+
