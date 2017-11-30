9dd1a92dbe87ca1d084097793ed1bc304d179992
josecolicoy <jcolicoy2015@alu.uct.cl>
2017-11-30 11:15:30 -0300

    nueva

--- matriculas (2).sql
+++ matriculas (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-11-2017 a las 13:59:02
+-- Tiempo de generación: 30-11-2017 a las 15:13:42
 -- Versión del servidor: 10.1.25-MariaDB
 -- Versión de PHP: 5.6.31
 
